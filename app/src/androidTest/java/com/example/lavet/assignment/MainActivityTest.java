7436cab908f45dcb311de2ffd76cb02006a0b94d
John Laveta <lavetajm@gmail.com>
2018-04-08 17:58:46 -0700

    Initial commit

---
+++ app/src/androidTest/java/com/example/lavet/assignment/MainActivityTest.java
@@ -0,0 +1,46 @@
+package com.example.lavet.assignment;
+
+import android.support.test.*;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> testRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void tester(){
+        //Test for initial greeting
+        onView(withId(R.id.textView))
+                .check(matches(withText(R.string.greeting)));
+
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+
+        //Test clicking on "About John" menu item, ensure proper changes made to textView
+        onView(withText(R.string.menu_item2)).perform(click());
+
+        onView(withId(R.id.textView))
+                .check(matches(withText(R.string.about_john)));
+
+        //Clicks back to home and rechecks textView for the greeting
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+
+        onView(withText(R.string.menu_item1)).perform(click());
+
+        onView(withId(R.id.textView))
+                .check(matches(withText(R.string.greeting)));
+    }
+
+}
---
+++ app/src/main/java/com/example/lavet/assignment/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.lavet.assignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        imageView = findViewById(R.id.headshotimg);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.about_john) {
+            textView.setText(R.string.about_john);
+            int imgRes = getResources().getIdentifier("@drawable/hs", null, this.getPackageName());
+            imageView.setImageResource(imgRes);
+            return true;
+        }
+
+        if(item.getItemId() == R.id.home) {
+            textView.setText(R.string.greeting);
+            imageView.setImageResource(0);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
