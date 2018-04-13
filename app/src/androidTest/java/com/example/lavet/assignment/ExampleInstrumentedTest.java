09dcb320716d5663b8a64e54a61baa299d16d25c
John Laveta <lavetajm@gmail.com>
2018-04-12 19:48:50 -0700

    Corrected package names

--- app/src/androidTest/java/com/example/lavet/assignment/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/lavet/assignment/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.lavet.inclassdemo2", appContext.getPackageName());
+        assertEquals("com.example.lavet.assignment", appContext.getPackageName());
     }
 }
