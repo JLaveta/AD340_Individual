d4cc9610b32bf65fffebe6cbeff0f8ff7ee51ff1
John Laveta <lavetajm@gmail.com>
2018-04-16 18:15:47 -0700

    Assignment 2 - Form created, calendar dialog box functional. Not currently passing anything to a second activity.

--- app/src/main/java/com/example/lavet/assignment/MainActivity.java
+++ app/src/main/java/com/example/lavet/assignment/MainActivity.java
@@ -1,26 +1,31 @@
 package com.example.lavet.assignment;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.ImageView;
-import android.content.Intent;
-import android.view.View;
+
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
     private ImageView imageView;
+    private DatePickerDialog.OnDateSetListener onDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.textView);
-        imageView = findViewById(R.id.headshotimg);
     }
 
     @Override
@@ -32,27 +37,48 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == R.id.about_john) {
-            textView.setText(R.string.about_john);
+            setContentView(R.layout.about);
+            imageView = findViewById(R.id.headshotimg);
             int imgRes = getResources().getIdentifier("@drawable/hs", null, this.getPackageName());
             imageView.setImageResource(imgRes);
             return true;
         }
 
         if(item.getItemId() == R.id.home) {
-            textView.setText(R.string.greeting);
-            imageView.setImageResource(0);
+            setContentView(R.layout.activity_main);
             return true;
         }
 
         if(item.getItemId() == R.id.register) {
-            goToRegistration();
+            setContentView(R.layout.form);
+            textView = findViewById(R.id.dob);
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Calendar cal = Calendar.getInstance();
+                    int year = cal.get(Calendar.YEAR);
+                    int month = cal.get(Calendar.MONTH);
+                    int day = cal.get(Calendar.DAY_OF_MONTH);
+                    DatePickerDialog datePick = new DatePickerDialog(
+                            MainActivity.this,
+                            android.R.style.Theme_Holo_Light_DarkActionBar,
+                            onDateSetListener,
+                            year, month, day);
+                    datePick.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                    datePick.show();
+
+                }
+            });
+
+            onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    String date = month+1 + "/" + dayOfMonth + "/" + year;
+                    textView.setText(date);
+                }
+            };
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    public void goToRegistration() {
-        Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-        startActivity(intent);
     }
 }
