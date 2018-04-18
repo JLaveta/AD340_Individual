02b7dcde1f50f510e4a78a9a256df8e4c242e86e
John Laveta <lavetajm@gmail.com>
2018-04-17 17:26:13 -0700

    Age checking added. Still need to handle rotation and write tests.

--- app/src/main/java/com/example/lavet/assignment/MainActivity.java
+++ app/src/main/java/com/example/lavet/assignment/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,6 +21,10 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
+    private int year;
+    private int day;
+    private int month;
+    private int dayOfYear;
     private ImageView imageView;
     private EditText editText;
     private DatePickerDialog.OnDateSetListener onDateSetListener;
@@ -61,9 +66,10 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     Calendar cal = Calendar.getInstance();
-                    int year = cal.get(Calendar.YEAR);
-                    int month = cal.get(Calendar.MONTH);
-                    int day = cal.get(Calendar.DAY_OF_MONTH);
+                    year = cal.get(Calendar.YEAR);
+                    month = cal.get(Calendar.MONTH);
+                    day = cal.get(Calendar.DAY_OF_MONTH);
+                    dayOfYear = cal.get(Calendar.DAY_OF_YEAR);
                     DatePickerDialog datePick = new DatePickerDialog(
                             MainActivity.this,
                             android.R.style.Theme_Holo_Light_DarkActionBar,
@@ -78,8 +84,35 @@ public class MainActivity extends AppCompatActivity {
             onDateSetListener = new DatePickerDialog.OnDateSetListener() {
                 @Override
                 public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    //Print out date of birth in placeholder
                     String date = month+1 + "/" + dayOfMonth + "/" + year;
                     textView.setText(date);
+
+                    //Calculate user's age based on input DOB
+                    int age = Calendar.getInstance().get(Calendar.YEAR) - year;
+                    if(Calendar.getInstance().get(Calendar.DAY_OF_YEAR) < dayOfYear){
+                        age--;
+                    }
+
+                    //Convert calculated age to a string format
+                    Integer ageInt = new Integer(age);
+                    String ageS = ageInt.toString();
+
+                    //Changes text and enables or disables the registration button based on age
+                    //Users under 18 cannot register
+                    TextView ageText = findViewById(R.id.age);
+                    Button regButton = findViewById(R.id.registrationButton);
+
+                    if(ageInt >= 18){
+                        ageText.setText(getString(R.string.age) + ageS);
+                        findViewById(R.id.registrationButton).setEnabled(true);
+                    }
+
+                    else if(ageInt < 18){
+                        ageText.setText(getString(R.string.age) + ageS);
+                        regButton.setText(getString(R.string.young));
+                        regButton.setEnabled(false);
+                    }
                 }
             };
             return true;
