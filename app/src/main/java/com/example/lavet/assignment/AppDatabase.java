104f118304e1f63dda26bd8c1c1ad01e87783ace
JLaveta <lavetajm@gmail.com>
2018-05-24 17:39:26 -0700

    Some progress. onPostExecutes currently crash app.

--- app/src/main/java/com/example/lavet/assignment/AppDatabase.java
+++ app/src/main/java/com/example/lavet/assignment/AppDatabase.java
@@ -1,7 +1,9 @@
 package com.example.lavet.assignment;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 import com.example.lavet.assignment.dao.SettingsDao;
 import com.example.lavet.assignment.entity.Settings;
@@ -10,4 +12,15 @@ import com.example.lavet.assignment.entity.Settings;
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract SettingsDao settingsDao();
+    private static AppDatabase db;
+
+    public static AppDatabase getDatabase(Context context) {
+        if(db == null) {
+            db = Room.databaseBuilder(context,
+                    AppDatabase.class, "settings")
+                    .build();
+        }
+
+        return db;
+    }
 }
--- app/src/main/java/com/example/lavet/assignment/SettingsFragment.java
+++ app/src/main/java/com/example/lavet/assignment/SettingsFragment.java
@@ -1,6 +1,8 @@
 package com.example.lavet.assignment;
 
+import android.app.Activity;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
@@ -15,18 +17,22 @@ import android.widget.Toast;
 
 import com.example.lavet.assignment.entity.Settings;
 
+import java.lang.ref.WeakReference;
+import java.util.List;
+
 /**
  * Provides UI for the view with List.
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements View.OnClickListener {
 
-    private EditText reminderTime;
-    private EditText maxDistance;
-    private EditText userGender;
-    private EditText privacySet;
-    private EditText minAge;
-    private EditText maxAge;
-    private Button submitButton;
+    public EditText reminderTime;
+    public EditText maxDistance;
+    public EditText userGender;
+    public EditText privacySet;
+    public EditText minAge;
+    public EditText maxAge;
+    public Button submitButton;
+    public View setView;
     Settings settings = new Settings();
 
 
@@ -34,7 +40,7 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View setView = inflater.inflate(R.layout.activity_profile_fragment, null);
+        setView = inflater.inflate(R.layout.activity_settings_fragment, null);
         reminderTime = setView.findViewById(R.id.reminderTime);
         maxDistance = setView.findViewById(R.id.maxDistance);
         userGender = setView.findViewById(R.id.gender);
@@ -43,34 +49,113 @@ public class SettingsFragment extends Fragment {
         maxAge = setView.findViewById(R.id.maxAge);
         submitButton = setView.findViewById(R.id.submitButton);
 
-        submitButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                settings.setGender(userGender.getText().toString());
-                settings.setMaxAge(Integer.parseInt(maxAge.getText().toString()));
-                settings.setMinAge(Integer.parseInt(minAge.getText().toString()));
-                settings.setMiles(Integer.parseInt(maxDistance.getText().toString()));
-                settings.setRemTime(reminderTime.getText().toString());
-                settings.setPrivacy(privacySet.getText().toString());
+        submitButton.setOnClickListener(this);
 
-                Context context = getContext();
-                int duration = Toast.LENGTH_SHORT;
-                String text = getString(R.string.setSubmit);
-
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.setGravity(Gravity.CENTER, 0, 0);
-                toast.show();
-            }
-        });
-
-        reminderTime.setText(settings.getRemTime());
-        maxDistance.setText(settings.getMiles());
-        userGender.setText(settings.getGender());
-        privacySet.setText(settings.getPrivacy());
-        minAge.setText(settings.getMinAge());
-        maxAge.setText(settings.getMaxAge());
+        new GetSettingsTask(this.getActivity(), settings).execute();
 
         return setView;
     }
 
+    @Override
+    public void onClick(View v){
+        settings.setGender(userGender.getText().toString());
+        settings.setMaxAge(Integer.parseInt(maxAge.getText().toString()));
+        settings.setMinAge(Integer.parseInt(minAge.getText().toString()));
+        settings.setMiles(Integer.parseInt(maxDistance.getText().toString()));
+        settings.setRemTime(reminderTime.getText().toString());
+        settings.setPrivacy(privacySet.getText().toString());
+
+        Context context = getContext();
+        int duration = Toast.LENGTH_SHORT;
+        String text = getString(R.string.setSubmit);
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+
+        new UpdateSettingsTask(this.getActivity(), settings).execute();
+    }
+
+    private class GetSettingsTask extends AsyncTask<Void, Void, Settings> {
+
+        private WeakReference<Activity> weakActivity;
+        private Settings settings;
+
+        public GetSettingsTask(Activity activity, Settings settings) {
+            weakActivity = new WeakReference<>(activity);
+            this.settings = settings;
+        }
+
+        @Override
+        protected Settings doInBackground(Void... voids) {
+            Activity activity = weakActivity.get();
+            if(activity == null) {
+                return null;
+            }
+
+            AppDatabase db = AppDatabase.getDatabase(activity.getApplicationContext());
+
+            List<Settings> settings = db.settingsDao().getAll();
+
+            if(settings.size() <= 0 || settings.get(0) == null) {
+                return null;
+            }
+            return settings.get(0);
+        }
+
+        @Override
+        protected void onPostExecute(Settings settings) {
+            Activity activity = weakActivity.get();
+            if(settings == null || activity == null) {
+                return;
+            }
+
+            reminderTime.setText(settings.getRemTime());
+            maxDistance.setText(settings.getMiles());
+            userGender.setText(settings.getGender());
+            privacySet.setText(settings.getPrivacy());
+            minAge.setText(settings.getMinAge());
+            maxAge.setText(settings.getMaxAge());
+        }
+    }
+
+    private class UpdateSettingsTask extends AsyncTask<Void, Void, Settings> {
+
+        private WeakReference<Activity> weakActivity;
+        private Settings settings;
+
+        public UpdateSettingsTask(Activity activity, Settings settings) {
+            weakActivity = new WeakReference<>(activity);
+            this.settings = settings;
+        }
+
+        @Override
+        protected Settings doInBackground(Void... voids) {
+            Activity activity = weakActivity.get();
+            if(activity == null) {
+                return null;
+            }
+
+            AppDatabase db = AppDatabase.getDatabase(activity.getApplicationContext());
+
+            db.settingsDao().insertAll(settings);
+            return settings;
+        }
+
+        @Override
+        protected void onPostExecute(Settings settings) {
+            Activity activity = weakActivity.get();
+            if(settings == null || activity == null) {
+                return;
+            }
+
+            reminderTime.setText(settings.getRemTime());
+            maxDistance.setText(settings.getMiles());
+            userGender.setText(settings.getGender());
+            privacySet.setText(settings.getPrivacy());
+            minAge.setText(settings.getMinAge());
+            maxAge.setText(settings.getMaxAge());
+        }
+    }
 
 }
